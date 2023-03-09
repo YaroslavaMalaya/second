46b975e37af0721b806c20a716fca7f67c6b2371
Katherine Shashkina <kshashkina@kse.org.ua>
2023-03-10 00:05:50 +0200

    added movement to finish

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -32,5 +32,135 @@ List<Point> GetShortestPath(string[,] map, Point start, Point goal)
         }   
     }
 
+    while (neighbours.Count > 0)
+    {
+        if (neighbours.Count == 1)
+        {
+            var point = neighbours.Dequeue();
+            foreach (var n in generator.GetNeighbours(point.Column, point.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+        }
+    
+        if (neighbours.Count == 2)
+        {
+            var point_1 = neighbours.Dequeue();
+            var point_2 = neighbours.Dequeue();
+            foreach (var n in generator.GetNeighbours(point_1.Column, point_1.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_2.Column, point_2.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+        }
+        
+        if (neighbours.Count == 3)
+        {
+            var point_1 = neighbours.Dequeue();
+            var point_2 = neighbours.Dequeue();
+            var point_3 = neighbours.Dequeue();
+            foreach (var n in generator.GetNeighbours(point_1.Column, point_1.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_2.Column, point_2.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_3.Column, point_3.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+        }
+        
+        if (neighbours.Count == 4)
+        {
+            var point_1 = neighbours.Dequeue();
+            var point_2 = neighbours.Dequeue();
+            var point_3 = neighbours.Dequeue();
+            var point_4 = neighbours.Dequeue();
+            foreach (var n in generator.GetNeighbours(point_1.Column, point_1.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_2.Column, point_2.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_3.Column, point_3.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+            foreach (var n in generator.GetNeighbours(point_4.Column, point_4.Row, map, 1))
+            {
+                if (!visited.Contains(n))
+                {
+                    neighbours.Enqueue(n);
+                    visited.Add(n);
+                    Console.WriteLine((n.Row, n.Column));
+                }
+            }
+        }
+    }
+    if (visited.Contains(goal))
+    {
+        Console.WriteLine("goal");
+    }
+
     return visited; // щоб червоним не підкреслювало поки не написана програма
 }
+
+
+//GetShortestPath(map, start, goal);
+//foreach (var element in GetShortestPath(map, start, goal))
+{
+    //Console.WriteLine((element.Row, element.Column));
+}            
